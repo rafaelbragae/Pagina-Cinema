82036de3f0470b786ec8d78bafda94a9acf89e37
Rafael Braga <rafaelbragae@gmail.com>
2022-09-02 10:42:34 -0300

    first commit

---
+++ cinemaedicao.js
@@ -0,0 +1,3 @@
+const nome = prompt ("Qual seu nome?");
+alert(`Seja bem vindo ${nome}!`);
+document.querySelector('#estilo').textContent = (`Olá ${nome}! qual estilo de filme você está buscando?`);
