6a7567443fc1533e893b844a14a4fb6fb4680ffb
Rafael Braga <rafaelbragae@gmail.com>
2022-09-02 10:45:39 -0300

    teste merge

--- cinemaedicao.js
+++ cinemaedicao.js
@@ -1,3 +1,3 @@
 const nome = prompt ("Qual seu nome?");
 alert(`Seja bem vindo ${nome}!`);
-document.querySelector('#estilo').textContent = (`Olá ${nome}! qual estilo de filme você está buscando?`);
+document.querySelector('#estilo').textContent = (`Oi ${nome}! qual estilo de filme você está buscando?`);
